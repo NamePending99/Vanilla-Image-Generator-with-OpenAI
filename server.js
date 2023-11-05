bcb2ad15980d6fa7f197a7be480d685878d1397d
Mori Adrian Rosland <mori@live.no>
2023-11-05 12:42:55 +0100

    Created API endpoint

---
+++ server.js
@@ -0,0 +1,28 @@
+import { Configuration, OpenAIApi } from 'openai';
+import express from 'express';
+import cors from cors;
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_KEY,
+})
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post('/dream', async (request, response) => {
+    const prompt = request.body.prompt;
+
+    const aiResponse = await openai.createImage({
+        prompt,
+        n: 1,
+        size: '1024x1024',
+    })
+
+    const image = aiResponse.data.data[0].url;
+    response.send({ image });
+});
